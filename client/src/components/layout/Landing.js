86f219abcc94dcf983f3b09ee8da5f0c88a231a8
Nathan Henniges <demonwolf@demonwolfdev.com>
2018-07-27 11:02:50 -0500

    Added errors handleing display for register.

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class Landing extends Component {
   render() {
@@ -12,8 +12,8 @@ class Landing extends Component {
                 </h1>
                 <p className="lead"> Create a developer profile/portfolio, share posts and get help from other developers</p>
                 <hr />
-                <a href="register.html" class="btn btn-lg btn-info mr-2">Sign Up</a>
-                <a href="login.html" class="btn btn-lg btn-light">Login</a>
+                <a href="register.html" className="btn btn-lg btn-info mr-2">Sign Up</a>
+                <a href="login.html" className="btn btn-lg btn-light">Login</a>
               </div>
             </div>
           </div>
