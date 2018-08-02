309c5b297fd270189ba1e91194d3265a925eb68d
Nathan Henniges <demonwolf@demonwolfdev.com>
2018-08-02 09:17:23 -0500

    Implemented redux, action, reducer workflow.

--- validation/register.js
+++ validation/register.js
@@ -34,7 +34,7 @@ module.exports = function validateRegisterInput(data) {
   }
 
   if (Validator.isEmpty(data.password2)) {
-    errors.password2 = 'Confirm Password field is required';
+    errors.password2 = 'Confirm password field is required';
   }
 
   if (!Validator.equals(data.password, data.password2)) {
